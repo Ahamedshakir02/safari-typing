cd956146b8cf65326c1010e33906b37f09002fdb
ahamedshakir02 <ahamedshakir02@gmail.com>
2026-06-23 16:57:06 +0530

    deleted all

--- src/components/Seo.jsx
+++
@@ -1,7 +0,0 @@
-import { useDocumentMeta } from '../lib/useDocumentMeta.js'
-
-/** Declarative head management. Renders nothing. */
-export default function Seo(props) {
-  useDocumentMeta(props)
-  return null
-}
